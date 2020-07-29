2f60b43e8fd9a985454221ace1ef606b40249e81
Andrea <andryx1994@gmail.com>
2020-07-29 23:49:22 +0200

    Versione 0.1
    Inizializzato progetto

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Boolean</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  </head>
+  <body>
+
+    @include('partials.header')
+
+    <h1>Main</h1>
+
+    @include('partials.footer')
+
+  </body>
+</html>
